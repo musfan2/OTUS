9a3ccd00a5da64ed8b87aeee9990818e2b2b33e5
DoctorS <petuhov@bolid.ru>
2023-03-27 17:37:11 +0300

    Первый Unit-тест написан!

--- QuadraticEquation.dpr
+++ QuadraticEquation.dpr
@@ -5,32 +5,7 @@
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, JSON, System.IOUtils, DateUtils, Math,
-  System.Generics.Collections;
-
-function Solve(A, B, C: Double): TList<Double>;
-var
-  D, X1, X2: Double;
-begin
-  Result := TList<Double>.Create;
-
-  D := ((B * B) - (4 * A * C));
-  if D < 0 then
-    WriteLn('Действительных корней нет')
-  else if D = 0 then
-  begin
-    X1 := (-B / (2 * A));
-    Result.Add(X1);
-    WriteLn('Один действительный корень Х = ' + FloatToStr(X1));
-  end
-  else
-  begin
-    X1 := ((-B + sqrt(D)) / (2 * A));
-    X2 := ((-B - sqrt(D)) / (2 * A));
-    Result.Add(X1);
-    Result.Add(X2);
-    WriteLn('Два корня Х1 = ' + FloatToStr(X1) + ', X2 = ' + FloatToStr(X2));
-  end;
-end;
+  System.Generics.Collections, UFunctions;
 
 begin
   try
---
+++ UQuadraticEquationTest.pas
@@ -0,0 +1,37 @@
+﻿unit UQuadraticEquationTest;
+
+interface
+
+uses
+  DUnitX.TestFramework, System.Generics.Collections, UFunctions;
+
+type
+
+  [TestFixture]
+  TMyTestObject = class
+  public
+    // Тест, который проверяет, что для уравнения x^2+1 = 0 корней нет (возвращается пустой массив)
+    [Test]
+    [TestCase('TestA', '1,0,1')]
+    procedure Test1(const A: Double; const B: Double; const C: Double);
+  end;
+
+implementation
+
+procedure TMyTestObject.Test1(const A: Double; const B: Double; const C: Double);
+var Res: TList<Double>;
+begin
+  Res := Solve(A, B, C);
+  if Res.Count = 0 then
+    Assert.Pass('OK!')
+  else
+    Assert.Fail('Найдены корни, там где их нет!')
+end;
+
+
+
+initialization
+
+TDUnitX.RegisterTestFixture(TMyTestObject);
+
+end.
